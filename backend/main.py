fac7c47507347771a7c0a5a44942b88e9e72017d
anamul94 <anamul.ice14@gmail.com>
2025-10-07 19:32:28 +0600

    fix websocket

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
-from src.presentation.api.routes import app
+from src.presentation.api.routes import socket_app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(socket_app, host="0.0.0.0", port=8000)
